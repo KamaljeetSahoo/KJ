48f09680ae7fc528beda7a925f3d23cbf6d60dd0
Kamaljeet <kamaljeetsahoo@gmail.com>
2021-09-01 21:36:03 +0530

    Header Animation Experiments and Project section started

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
 import Typical from 'react-typical'
+import { bounce } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
 import Switch from "react-switch";
+import ProjectDetails from './ProjectDetails';
+// import Particles from 'react-particles-js';
 
 
 const Header = () => {
@@ -11,6 +15,8 @@ const Header = () => {
     const HeaderTitleAnimation = React.memo( () => {
         return <Typical className="title-styles" steps={titles} loop={300} />
     }, (props, prevProps) => true);
+
+
     const handleThemeChange = () => {
         setChecked(!checked)
         var dataThemeAttribute = "data-theme";
@@ -18,13 +24,94 @@ const Header = () => {
         var newTheme = body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
         body.setAttribute(dataThemeAttribute, newTheme);
     }
+
+    const styles = {
+        bounce: {
+          animation: 'x 1s',
+          animationName: Radium.keyframes(bounce, 'bounce'),
+          display: 'inline-block',
+          height: '100'
+        }
+      }
+
     return (
         <div>
             <header id="home" style={{ height: window.innerHeight, display: 'block' }}>
                 <div className="row aligner" style={{height: '100%'}}>
                     <div className="col-md-12">
                         <div>
-                            <span className="iconify header-icon" data-icon="carbon:user-activity" data-inline="false"></span>
+                            <StyleRoot>
+                                <div style={styles.bounce}>
+                                    <span className="iconify header-icon" data-icon="carbon:user-activity" data-inline="false"></span>
+                                    {/* <div className="">
+                                        <Particles height="100%" width="100%"
+                                        params={{
+                                            "fps_limit": 10,
+                                            "particles": {
+                                                "collisions": {
+                                                    "enable": true
+                                                },
+                                                "number": {
+                                                    "value": 180,
+                                                    "density": {
+                                                        "enable": false
+                                                    }
+                                                },
+                                                "line_linked": {
+                                                    "enable": true,
+                                                    "distance": 30,
+                                                    "opacity": 0.4
+                                                },
+                                                "move": {
+                                                    "speed": 1
+                                                },
+                                                "opacity": {
+                                                    "anim": {
+                                                        "enable": true,
+                                                        "opacity_min": 0.05,
+                                                        "speed": 1,
+                                                        "sync": false
+                                                    },
+                                                    "value": 0.4
+                                                }
+                                            },
+                                            "polygon": {
+                                                "enable": true,
+                                                "scale": 0.5,
+                                                "type": "inline",
+                                                "move": {
+                                                    "radius": 10
+                                                },
+                                                "url": "shaolin.svg",
+                                                "inline": {
+                                                    "arrangement": "equidistant"
+                                                },
+                                                "draw": {
+                                                    "enable": true,
+                                                    "stroke": {
+                                                        "color": "rgba(0, 0, 0, .2)"
+                                                    }
+                                                }
+                                            },
+                                            "retina_detect": false,
+                                            "interactivity": {
+                                                "events": {
+                                                    "onhover": {
+                                                        "enable": true,
+                                                        "mode": "repulse"
+                                                    }
+                                                },
+                                                "modes": {
+                                                    "bubble": {
+                                                        "size": 6,
+                                                        "distance": 40
+                                                    }
+                                                }
+                                            }
+                                        }} />
+                                    </div> */}
+                                </div>
+                            </StyleRoot>
                             <br/>
                             <h1>
                                 <Typical steps={['Kamaljeet']} wrapper="p" />
---
+++ src/components/ProjectDetails.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Modal } from 'react-bootstrap'
+import AwesomeSlider from 'react-awesome-slider'
+import AwesomeSliderStyles from "../scss/light-slider.scss";
+import AwesomeSliderStyles2 from "../scss/dark-slider.scss";
+import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
+
+const ProjectDetails = (props) => {
+
+    var technologies = props.data.technologies
+    var images = props.data.images
+    var title = props.data.title
+    var description = props.data.description
+    var url = props.data.url
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProjectDetails
